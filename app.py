7578a40117dceb3d421c091176796724903cefc9
Rostyslav Pudlo <rostyslav.pudlo@gmail.com>
2024-08-18 15:04:25 +0300

    prepare iterated items to dispaly

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import logging
 from datetime import UTC, datetime
+from itertools import chain
 from pathlib import Path
 from typing import Iterable
 
@@ -12,22 +13,25 @@ from bs4 import BeautifulSoup, Tag
 logger = logging.getLogger(__name__)
 
 
-class ShortstoryData(pydantic.BaseModel):
+class Item(pydantic.BaseModel):
     id_: int
     title: str
     update_date: datetime
-    update_marker: str
+    update_marker: str | None
     url: str
+
+    priority: float | None = None
+    # TODO: use this to store own description. `description: str | None = None`
 
 
 class CachedData(pydantic.BaseModel):
-    items: list[ShortstoryData] = []
+    items: list[Item] = []
 
 
 cached_data_file_path = Path("cached_data.json")
 
 
-def _load_cached_data() -> dict[int, ShortstoryData]:
+def _load_cached_data() -> dict[int, Item]:
     if not cached_data_file_path.exists():
         return {}
 
@@ -44,7 +48,7 @@ def _save_cached_data() -> None:
         data_file.write(CachedData(items=list(cached_data.values())).model_dump_json())
 
 
-def _add_to_cached_data(item: ShortstoryData) -> bool:
+def _add_to_cached_data(item: Item) -> bool:
     cached_item = cached_data.get(item.id_)
     if cached_item is not None and cached_item.update_marker == item.update_marker:
         return False
@@ -55,8 +59,8 @@ def _add_to_cached_data(item: ShortstoryData) -> bool:
 
 
 class Data(pydantic.BaseModel):
-    items: list[ShortstoryData] = []
-    ignored_items: list[ShortstoryData] = []
+    items: list[Item] = []
+    ignored_items: list[Item] = []
 
 
 # TODO: use datavase (sqlite + sqlalchemy + orm) to store data and data_ignore
@@ -64,7 +68,7 @@ class Data(pydantic.BaseModel):
 data_file_path = Path("data.json")
 
 
-def _load_data() -> tuple[dict[int, ShortstoryData], dict[int, ShortstoryData]]:
+def _load_data() -> tuple[dict[int, Item], dict[int, Item]]:
     if not data_file_path.exists():
         return {}, {}
 
@@ -74,10 +78,10 @@ def _load_data() -> tuple[dict[int, ShortstoryData], dict[int, ShortstoryData]]:
         items = {item.id_: item for item in data.items}
         ignored_items = {item.id_: item for item in data.ignored_items}
 
-        for item in data.items:
-            _add_to_cached_data(item)
-        for item in data.ignored_items:
-            _add_to_cached_data(item)
+        for item in chain(data.items, data.ignored_items):
+            if item.id_ not in cached_data:
+                _add_to_cached_data(item)
+                cached_data[item.id_].update_marker = None
 
         return items, ignored_items
 
@@ -105,7 +109,7 @@ def _parse_shortstory_data_s(
     soup: BeautifulSoup,
     *,
     stop_on_duplicate: bool = False,
-) -> Iterable[ShortstoryData]:
+) -> Iterable[Item]:
     for shortstory_element in soup.find_all(class_="shortstory"):
         if not isinstance(shortstory_element, Tag):
             raise TypeError
@@ -127,7 +131,7 @@ def _parse_shortstory_data_s(
             raise TypeError
         update_marker = update_marker_element.text
 
-        item = ShortstoryData(
+        item = Item(
             id_=id_,
             title=title,
             update_date=datetime.now(tz=UTC),
@@ -153,7 +157,7 @@ def _parse_main_page(
     page_index: int = 1,
     *,
     stop_on_duplicate: bool = False,
-) -> Iterable[ShortstoryData]:
+) -> Iterable[Item]:
     url = "https://v2.vost.pw/"
     if page_index > 1:
         url = f"{url}page/{page_index}/"
@@ -168,7 +172,7 @@ def _parse_main_pages(
     page_count: int = 10,
     *,
     stop_on_duplicate: bool = False,
-) -> Iterable[ShortstoryData]:
+) -> Iterable[Item]:
     if stop_on_duplicate and len(cached_data) == 0:
         yield from _parse_main_page()
         return
@@ -195,7 +199,7 @@ def _parse_shortstory_page(id_: int) -> None:
         raise TypeError
     update_marker = update_marker_element.text
 
-    shortstory_data = ShortstoryData(
+    shortstory_data = Item(
         id_=id_,
         title=title,
         update_date=datetime.now(tz=UTC),
@@ -221,6 +225,69 @@ def _add_ignore(id_: int) -> None:
     _save_data()
 
 
+def _set_priority(id_: int, priority: float) -> None:
+    if id_ not in data:
+        return
+
+    data[id_].priority = priority
+    _save_data()
+
+
+# show 3 lists:
+#   - list of interest (loi):
+#       - filter by changes; i don't want to see information that i've processed;
+#       - sorted by priority; to see main information first; it would be good to store data in sorted mode;
+#       - show id, info (title), changes; id to manipulate, info to understand, changes to make desizion;
+#   - list of others;
+#       - filter (exclude loi; exclude ignor_list);
+#       - sort by update_date; it would be good to store data in sorted mode;
+#       - group by number of update (each update of main pages has unique number; not implemented);
+#       - show id, info (title); id to manipulate, info to understand;
+#   - list of ignore (igonre_list);
+#       - sorted by time of adding; to remove if add wrong; it would be good to store data in sorted mode;
+#       - show id, info (title);
+
+
+def _get_interests() -> Iterable[str]:
+    changed_data = (
+        item
+        for item in data.values()
+        if item.update_marker != cached_data[item.id_].update_marker
+    )
+    none_priority: float = float("inf")
+    sorted_data_by_priority = sorted(
+        changed_data,
+        key=lambda x: x.id_ if x is not None else none_priority,
+        reverse=True,
+    )
+    yield from (
+        f"{item.id_}\n\t{item.title}\n\t{cached_data[item.id_].title}"
+        for item in sorted_data_by_priority
+    )
+
+
+def _get_others() -> Iterable[str]:
+    other_data = [
+        value
+        for key, value in cached_data.items()
+        if key not in data and key not in ignored_data
+    ]
+    sorted_data_by_update_dt = sorted(
+        other_data, key=lambda x: x.update_date.astimezone(UTC), reverse=True,
+    )
+    yield from (f"{item.id_}\n\t{item.title}" for item in sorted_data_by_update_dt)
+
+
+def _get_wrong() -> Iterable[str]:
+    sorted_data_by_add_dt = ignored_data  # TODO: add field `add_dt` to use `sorted(ignored_data, key=lambda x: x.add_date.astimezone(UTC), reverse=True)`
+    yield from (
+        f"{item.id_}\n\t{item.title}" for item in sorted_data_by_add_dt.values()
+    )
+
+
+# TODO: add vs add_and_update
+# TODO: show stored difference between stored and cached. show diff in title.
+# TODO: update title and update_marker
 # TODO: add priority to interested items
 # TODO: show items wich is not equal to cached and sort by priority (use paging)
 # TODO: show cached items groupped by update_date sort
